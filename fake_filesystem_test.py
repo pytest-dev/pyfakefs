3cb13e439dd600e2d04a8dfc46d120d7b5dc8839
Filip Figiel <filip.figiel@dcs.pl>
2015-04-25 14:07:10 +0200

    Run most of unix tests on cygwin, add TestCase system attributes

--- fake_filesystem_test.py
+++ fake_filesystem_test.py
@@ -28,7 +28,6 @@ else:
     import unittest
 
 import fake_filesystem
-from fake_filesystem import is_windows
 
 
 def _GetDummyTime(start_time, increment):
@@ -40,6 +39,8 @@ def _GetDummyTime(start_time, increment):
 
 
 class TestCase(unittest.TestCase):
+  is_windows = sys.platform.startswith('win')
+  is_cygwin = sys.platform == 'cygwin'
 
   def assertModeEqual(self, expected, actual):
     return self.assertEqual(stat.S_IMODE(expected), stat.S_IMODE(actual))
@@ -1843,8 +1844,7 @@ class FakePathModuleTest(TestCase):
     self.assertEqual('.',
                      self.path.relpath(path_bar, path_bar))
 
-  @unittest.skipIf(is_windows,
-                   'realpath does not follow symlinks in win32')
+  @unittest.skipIf(TestCase.is_windows, 'realpath does not follow symlinks in win32')
   def testRealpathVsAbspath(self):
     self.filesystem.CreateFile('/george/washington/bridge')
     self.filesystem.CreateLink('/first/president', '/george/washington')
@@ -1880,12 +1880,16 @@ class FakePathModuleTest(TestCase):
     self.assertEqual('foo/bar/baz', self.path.join(*components))
 
   def testExpandUser(self):
-    if is_windows:
+    if self.is_windows:
       self.assertEqual(self.path.expanduser('~'),
                        self.os.environ['USERPROFILE'].replace('\\', '/'))
     else:
       self.assertEqual(self.path.expanduser('~'),
                        self.os.environ['HOME'])
+
+  @unittest.skipIf(TestCase.is_windows or TestCase.is_cygwin,
+                   'only tested on unix systems')
+  def testExpandRoot(self):
       self.assertEqual('/root', self.path.expanduser('~root'))
 
   def testGetsizePathNonexistent(self):
@@ -1969,7 +1973,7 @@ class FakePathModuleTest(TestCase):
 
     self.assertFalse(self.path.islink('it_dont_exist'))
 
-  @unittest.skipIf(sys.version_info >= (3, 0) or is_windows,
+  @unittest.skipIf(sys.version_info >= (3, 0) or TestCase.is_windows,
                    'os.path.walk deprecrated in Python 3, cannot be properly '
                    'tested in win32')
   def testWalk(self):
@@ -1987,7 +1991,7 @@ class FakePathModuleTest(TestCase):
                 ('/foo/bar/xyzzy', 'plugh')]
     self.assertEqual(expected, visited_nodes)
 
-  @unittest.skipIf(sys.version_info >= (3, 0) or is_windows,
+  @unittest.skipIf(sys.version_info >= (3, 0) or TestCase.is_windows,
                    'os.path.walk deprecrated in Python 3, cannot be properly '
                    'tested in win32')
   def testWalkFromNonexistentTopDoesNotThrow(self):
