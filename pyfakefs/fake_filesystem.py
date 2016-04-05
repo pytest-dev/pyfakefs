617afe6c798faa56efad5a7c3934310cd2fdb5d6
John McGehee <github@johnnado.com>
2016-04-04 21:54:17 -0700

    Accept pull request 71 and 74
    71:Adding followlinks parameter to os.walk includes test
    74: Add support for os.fsync() and os.fdatasync() with corresponding tests

--- pyfakefs/fake_filesystem.py
+++ pyfakefs/fake_filesystem.py
@@ -98,7 +98,7 @@ except ImportError:
 
 __pychecker__ = 'no-reimportself'
 
-__version__ = '2.7'
+__version__ = '2.8'
 
 PERM_READ = 0o400      # Read permission bit.
 PERM_WRITE = 0o200     # Write permission bit.
