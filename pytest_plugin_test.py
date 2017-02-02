93436193eae7a75a2419649a1016f0eb258ec9e8
Wes Kendall <wes.kendall@cloverhealth.com>
2017-02-02 15:03:41 -0800

    removed dummy test

--- pytest_plugin_test.py
+++ pytest_plugin_test.py
@@ -2,17 +2,7 @@
 import os
 
 
-import pytest
-
-
-@pytest.mark.parametrize('f', ['/var/data/xx1.txt', '/var/data/xx1.txt'])
 def test_fs_fixture(fs, f):
     assert not os.path.exists(f)
     fs.CreateFile(f)
     assert os.path.exists(f)
-
-
-def test_fs_fixture2(fs):
-    assert not os.path.exists('/var/data/xx1.txt')
-    fs.CreateFile('/var/data/xx1.txt')
-    assert os.path.exists('/var/data/xx1.txt')
