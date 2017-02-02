0137d1b922bcaf60edd6528567879239fb13d278
Wes Kendall <wes.kendall@cloverhealth.com>
2017-02-02 13:16:54 -0800

    added pytest module

---
+++ pyfakefs/pytest_module.py
@@ -0,0 +1,15 @@
+import py
+import pytest
+from pyfakefs.fake_filesystem_unittest import Patcher
+
+
+Patcher.SKIPMODULES.add(py)  # Ignore pytest components when faking filesystem
+
+
+@pytest.yield_fixture
+def fs():
+    """ Fake filesystem. """
+    patcher = Patcher()
+    patcher.setUp()
+    yield patcher.fs
+    patcher.tearDown()
