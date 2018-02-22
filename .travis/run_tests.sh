8799bbf4231c50ad2063fdae7ef34fb9b5444879
cclauss <cclauss@bluewin.ch>
2018-02-22 06:44:32 +0100

    Add flake8 to testing
    * FileNotFoundError, FileExistsError, NotADirectoryError exist in Python 3 but not in Python 2.
    * file(), long, unicode exist in Python 2 but not in Python 3

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -5,4 +5,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 fi
 
 python --version
+pip --version
+pip install flake8
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
+# exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 python -m tests.all_tests
