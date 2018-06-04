929927a89c7e6f758ac3640a0946eb0b4704b474
mrbean-bremen <hansemrbean@googlemail.com>
2018-06-04 21:22:11 +0200

    Make sure that the linecache module is not using the fake file system
    - does not work for Python 3.3 (which should not be used anymore anyway)
    - see #381

---
+++ pyfakefs/tests/pytest_check_failed_plugin_test.py
@@ -0,0 +1,19 @@
+"""Tests that a failed pytest properly displays the call stack.
+Uses the output from running pytest with pytest_plugin_failing_test.py.
+Regression test for #381.
+"""
+import pytest
+import sys
+
+
+@pytest.mark.skipif(sys.version_info[0] == 3 and sys.version_info[1] == 3,
+                    reason='linecache uses fake fs in Python 3.3')
+def test_failed_testresult_stacktrace():
+    with open('testresult.txt') as f:
+        contents = f.read()
+    # before the fix, a triple question mark has been displayed
+    # instead of the stacktrace
+    assert contents
+    print('contents', contents)
+    assert not '???' in contents
+    assert 'def test_fs(fs):' in contents
---
+++ pyfakefs/tests/pytest_plugin_failing_test.py
@@ -0,0 +1,5 @@
+""" Failing test to test stacktrace output - see
+``pytest_check_failed_plugin_test.py``."""
+
+def test_fs(fs):
+    assert False
