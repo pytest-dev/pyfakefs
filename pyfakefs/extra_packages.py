042adeef6c865a085c31c4cf6c6cdab07c58bea8
mrbean-bremen <hansemrbean@googlemail.com>
2018-12-26 19:19:50 +0100

    Load external packages pathlib2 and scandir before internal packages
    - see #462

--- pyfakefs/extra_packages.py
+++ pyfakefs/extra_packages.py
@@ -1,24 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Imports external packages that replace or emulate internal packages.
+If the external module is not present, the build-in module is imported.
+"""
+
 try:
-    import pathlib
-    pathlib2 = None
+    import pathlib2
+    pathlib = None
 except ImportError:
     try:
-        import pathlib2
-        pathlib = pathlib2
+        import pathlib
+        pathlib2 = None
     except ImportError:
         pathlib = None
         pathlib2 = None
 
 
 try:
-    from os import scandir
+    import scandir
     use_scandir = True
-    use_scandir_package = False
+    use_scandir_package = True
 except ImportError:
     try:
-        import scandir
+        from os import scandir
         use_scandir = True
-        use_scandir_package = True
+        use_scandir_package = False
     except ImportError:
         use_scandir = False
         use_scandir_package = False
