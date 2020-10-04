48384191e4f116a81fff6c577fadbf80c7a04c7a
mrbean-bremen <hansemrbean@googlemail.com>
2020-10-04 14:36:56 +0200

    Fix missing import introduced by previous change

--- pyfakefs/pytest_tests/conftest.py
+++ pyfakefs/pytest_tests/conftest.py
@@ -21,11 +21,11 @@ import pytest
 from pyfakefs.fake_filesystem_unittest import Patcher
 
 # import the fs fixture to be visible if pyfakefs is not installed
+from pyfakefs.pytest_plugin import fs  # noqa: F401
 
 Patcher.SKIPMODULES.add(pytest)
 Patcher.SKIPMODULES.add(py)
 
-from pyfakefs.fake_filesystem_unittest import Patcher  # noqa: E402
 from pyfakefs.pytest_tests import example  # noqa: E402
 
 
