a8ea7b34330a28d359c8c8cb3da0ecd83b34609b
mrbean-bremen <hansemrbean@googlemail.com>
2020-11-01 10:16:59 +0100

    Release 4.2.0

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.2dev',
+    VERSION: '4.3dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
