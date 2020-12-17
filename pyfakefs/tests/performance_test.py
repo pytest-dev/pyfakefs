0ba2e0ebcc217ea9de1e7b89d1a2ee8ad6aaa8bc
mrbean-bremen <hansemrbean@googlemail.com>
2020-12-17 20:54:48 +0100

    Add test for checking the setup time
    - run performance tests only if configured per env var
    - times are taken from current CI tests with some margin
    - safeguard against performance degradation in setup

--- pyfakefs/tests/performance_test.py
+++ pyfakefs/tests/performance_test.py
@@ -10,31 +10,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Shall provide tests to check performance overhead of pyfakefs."""
-
+import os
+import time
 import unittest
 
 from pyfakefs.fake_filesystem_unittest import TestCase
+from pyfakefs.helpers import IS_PYPY
 
+if os.environ.get('TEST_PERFORMANCE'):
 
-class SetupPerformanceTest(TestCase):
-    def setUp(self) -> None:
-        self.setUpPyfakefs()
+    class SetupPerformanceTest(TestCase):
+        @classmethod
+        def setUpClass(cls) -> None:
+            cls.start_time = time.time()
 
+        @classmethod
+        def tearDownClass(cls) -> None:
+            cls.elapsed_time = time.time() - cls.start_time
+            print('Elapsed time per test for cached setup: {:.3f} ms'.format(
+                cls.elapsed_time * 10))
 
-class SetupNoCachePerformanceTest(TestCase):
-    def setUp(self) -> None:
-        self.setUpPyfakefs(use_cache=False)
+        def setUp(self) -> None:
+            self.setUpPyfakefs()
 
+    class SetupNoCachePerformanceTest(TestCase):
+        @classmethod
+        def setUpClass(cls) -> None:
+            cls.start_time = time.time()
 
-def test_setup(self):
-    pass
+        @classmethod
+        def tearDownClass(cls) -> None:
+            cls.elapsed_time = time.time() - cls.start_time
+            print('Elapsed time per test for uncached setup: {:.3f} ms'.format(
+                cls.elapsed_time * 10))
 
+        def setUp(self) -> None:
+            self.setUpPyfakefs(use_cache=False)
 
-for n in range(100):
-    test_name = "test_" + str(n)
-    setattr(SetupPerformanceTest, test_name, test_setup)
-    test_name = "test_nocache" + str(n)
-    setattr(SetupNoCachePerformanceTest, test_name, test_setup)
+    @unittest.skipIf(IS_PYPY, 'PyPy times are not comparable')
+    class TimePerformanceTest(TestCase):
+        """Make sure performance degradation in setup is noticed.
+        The numbers are related to the CI builds and may fail in local builds.
+        """
 
-if __name__ == "__main__":
-    unittest.main()
+        def test_cached_time(self):
+            self.assertLess(SetupPerformanceTest.elapsed_time, 0.4)
+
+        def test_uncached_time(self):
+            self.assertLess(SetupNoCachePerformanceTest.elapsed_time, 6)
+
+    def test_setup(self):
+        pass
+
+    for n in range(100):
+        test_name = "test_" + str(n)
+        setattr(SetupPerformanceTest, test_name, test_setup)
+        test_name = "test_nocache" + str(n)
+        setattr(SetupNoCachePerformanceTest, test_name, test_setup)
+
+    if __name__ == "__main__":
+        unittest.main()
